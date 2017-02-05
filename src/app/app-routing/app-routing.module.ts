fd41ef075cb0c67eb7ecc33b411857142c065482
Jeroen Druwe <druwe.jeroen@gmail.com>
2017-02-05 22:21:45 +0100

    Added page not found and some other styling

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule}             from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {TestComponent} from "../test/test.component";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
 
 const routes: Routes = [
   // {path: '', redirectTo: '/test', pathMatch: 'full'},
   {path: 'test', component: TestComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,18 @@ import {MaterialModule} from '@angular/material';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataServiceService }  from './in-memory-data-service.service';
 
+import 'hammerjs';
+
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {TestComponent} from './test/test.component';
-
-import 'hammerjs';
-import { DerpComponent } from './derp/derp.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    DerpComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
