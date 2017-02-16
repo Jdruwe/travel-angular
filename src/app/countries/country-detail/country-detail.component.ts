467302fd19c418243bf5f22e72feea37987f29f1
Jeroen Druwe <druwe.jeroen@gmail.com>
2017-02-16 14:40:40 +0100

    Added country id to parameters of detail view

--- src/app/countries/country-detail/country-detail.component.ts
+++ src/app/countries/country-detail/country-detail.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {CountryService} from "../country.service";
+
+import 'rxjs/add/operator/switchMap';
+import {Country} from "../country";
 
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.scss']
 })
-export class CountryDetailComponent implements OnInit {
+export class CountryDetailComponent implements OnInit{
 
-  constructor() { }
+  country: Country;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private countryService: CountryService) {
+  }
 
   ngOnInit() {
+    // (+) converts string 'id' to a number
+    this.route.params
+      .switchMap((params: Params) => this.countryService.getCountry(+params['id']))
+      .subscribe((country: Country) => this.country = country);
+  }
+
+  goToCountries(): void {
+    this.router.navigate(['/countries']);
   }
 
 }
--- src/app/countries/country.service.ts
+++ src/app/countries/country.service.ts
@@ -20,6 +20,13 @@ export class CountryService {
       .catch(CountryService.handleError);
   }
 
+  getCountry(id: number | string): Promise<Country> {
+    return this.http.get(`${this.countriesUrl}/${id}`)
+      .toPromise()
+      .then(response => response.json().data as Country)
+      .catch(CountryService.handleError);
+  }
+
   private static handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
